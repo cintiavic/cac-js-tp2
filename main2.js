9dcd9b0b5262adbc3c22e7f56b5614ba204637af
Cintia Vicente <cintia.huddle@gmail.com>
2024-05-02 08:53:25 -0300

    Commit inicial - Agrego hasta el ejercicio 4 inclusive

---
+++ main2.js
@@ -0,0 +1,23 @@
+/* EJ 1
+let num = 0;
+num = parseInt(prompt ("Ejercicio 1: Ingrese un número que le imprimo algo"));
+
+ej1(num); */
+
+/* EJ 2
+let n1;
+let n2;
+
+n1 = parseInt(prompt ("Ejercicio 2: Ingrese un número: "));
+n2 = parseInt(prompt ("Ejercicio 2: Ingrese otro número: "));
+ej2(n1,n2); */
+
+/* EJ 3
+let n1,n2,n3;
+n1 = parseInt(prompt ("Ejercicio 3: Ingrese un número: "));
+n2 = parseInt(prompt ("Ejercicio 3: Ingrese un número: "));
+n3 = parseInt(prompt ("Ejercicio 3: Ingrese un número: "));
+document.write("El promedio entre " + n1 + ", " + n2 + ", " +  n3 + " es: " + ej3(n1,n2,n3)); */
+
+/* EJ 4
+document.write("el promedio de notas es: " + ej4()); */
---
+++ script2.js
@@ -0,0 +1,42 @@
+function ej1(x){
+    let i;
+    for ( i = 1 ; i <= x ; i++){
+        document.write ("Bienvenidos al curso Full Stack");
+        document.write("<br>");
+    }
+}
+
+function ej2 (n1, n2) {
+    if (n1 < n2) {
+        document.write ("El mayor es " + n2)
+    }
+    else if (n1 > n2) {
+        document.write ("El mayor es " + n1)
+    }
+    else {
+        document.write ("Los números son iguales")
+    }
+}
+
+let ej3 = (n1, n2, n3) => (n1+n2+n3)/3;
+
+function ej4 () {
+    let acumulador = 0;
+    let nota;
+    let cantNotas = 1;
+    let promedio;
+
+    nota = parseInt(prompt("ingrese nota: "));
+
+    while (nota != -1){
+        acumulador += nota;
+        cantNotas++;
+        nota = parseInt(prompt("ingrese nota: "));
+    }
+
+    promedio = acumulador / (cantNotas-1);
+    return (promedio);
+}
+
+
+
